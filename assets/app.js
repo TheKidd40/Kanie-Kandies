b659eeab62f3bd47e60950e2a90377e8310648e1
Demetrius Walker <Demetriuswalker117@gmail.com>
2024-02-27 20:10:30 -0700

    made changes

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+console.log("JS is Alive!");
+// this is a comment 
+//use of the comsole
+
+//js variables
+//use variables to store values.
+//Declare, then initialize
+//ES5-Var
+//ES6-Let Const (constant)
+
+//const-constant
+//let-variable that will change their value. If it's not constant
+
+//Data types
+
+//strings 'Demetrius walker'-text enclosed in "double quotes"
+//numbers 22-decimals,#values,negative
+//boolean true false-yes or no, off or on asked like a yes/no
+//Arrays-a list of similar items [square brackets, separted by commas]
+//Unidentified-variable is unassigned
+//null-has no value now, but it will change later
+
+//Variable names
+//lower case
+//multi word-//
+//use camelCase-first letter of the word is lower case the second word is upper case (so anything after the first word is all upper case )
+//no spaces
+//do not start with #s or symbols
+//related to the info it's sharing
+//reserved words 
+// examples break null return location final(do not name variable's)
+//Practice
+
+//Create 5 variables name, age,city, have kids, fav movies/games
+//concatenate together and consoles.log it/browser objects 
+
+const myname="Demetrius Walker";
+let myage=39;
+let myloctation="Denver,Co";
+let havekids=true;
+let favMovies=["heat","Black Panther" ,"John Wick"];
+//ES5
+// console.log("my name is"+ myname+ "I am "+ myage+" myloctation +"Kids" + havekids + "My fav Movies" + favMovies);
+alert(`My name is ${myname} \n I live in ${myloctation}\n I am ${myage}`);
+// 
